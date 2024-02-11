70a7eb1bc32fb404d87d2f1871c6f2d833188b75
Dario Oddenino <branch13@gmail.com>
2024-02-11 12:32:34 +0100

    better compute

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,8 @@ const zm = @import("zmath");
 const Vertex = @import("cube_mesh.zig").Vertex;
 const vertices = @import("cube_mesh.zig").vertices;
 
+// FOLLOWING https://webgpufundamentals.org/webgpu/lessons/webgpu-compute-shaders.html
+// TODO workgroup returns as null, might have something to do with the buffer size?
 pub const App = @This();
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,8 +14,12 @@ const UniformBufferObject = struct {
     mat: zm.Mat,
 };
 
-const workgroup_size = 64;
-const buffer_size = 1000;
+const dispatch_count = 4 * 3 * 2;
+const num_workgroups = 4 * 3 * 2;
+const num_threads_per_workgroup = 2 * 3 * 4;
+const num_results = num_workgroups * num_threads_per_workgroup;
+const buffer_size = num_results;
+const size = num_results * @sizeOf(zm.Vec);
 
 title_timer: core.Timer,
 timer: core.Timer,
@@ -22,29 +28,68 @@ compute_pipeline: *gpu.ComputePipeline,
 compute_bind_group: *gpu.BindGroup,
 vertex_buffer: *gpu.Buffer,
 uniform_buffer: *gpu.Buffer,
-output_buffer: *gpu.Buffer,
-staging_buffer: *gpu.Buffer,
+// output_buffer: *gpu.Buffer,
+// staging_buffer: *gpu.Buffer,
 bind_group: *gpu.BindGroup,
-// staging_response: *gpu.Buffer.MapAsyncStatus,
+write_buffers: [3]*gpu.Buffer,
+read_buffers: [3]*gpu.Buffer,
 
 pub fn init(app: *App) !void {
     try core.init(.{});
 
-    const output = core.device.createBuffer(&.{
-        .label = "output buffer",
-        .usage = .{ .storage = true, .copy_src = true, .copy_dst = true },
-        .size = buffer_size * @sizeOf(f32),
+    const usage = .{ .storage = true, .copy_src = true };
+    const workgroup_buffer = core.device.createBuffer(&.{
+        .label = "workgroup buffer",
+        .usage = usage,
+        .size = size,
         .mapped_at_creation = .false,
     });
-
-    core.queue.writeBuffer(output, 0, &[_]f32{5} ** buffer_size);
-
-    const staging = core.device.createBuffer(&.{
-        .label = "staging buffer",
-        .usage = .{ .map_read = true, .copy_dst = true },
-        .size = buffer_size * @sizeOf(f32),
+    const local_buffer = core.device.createBuffer(&.{
+        .label = "local buffer",
+        .usage = usage,
+        .size = size,
         .mapped_at_creation = .false,
     });
+    const global_buffer = core.device.createBuffer(&.{
+        .label = "global buffer",
+        .usage = usage,
+        .size = size,
+        .mapped_at_creation = .false,
+    });
+    const read_usage = .{ .map_read = true, .copy_dst = true };
+    const workgroup_read_buffer = core.device.createBuffer(&.{
+        .label = "workgroup read buffer",
+        .usage = read_usage,
+        .size = size,
+        .mapped_at_creation = .false,
+    });
+    const local_read_buffer = core.device.createBuffer(&.{
+        .label = "local read buffer",
+        .usage = read_usage,
+        .size = size,
+        .mapped_at_creation = .false,
+    });
+    const global_read_buffer = core.device.createBuffer(&.{
+        .label = "global read buffer",
+        .usage = read_usage,
+        .size = size,
+        .mapped_at_creation = .false,
+    });
+    // const output = core.device.createBuffer(&.{
+    //     .label = "output buffer",
+    //     .usage = .{ .storage = true, .copy_src = true, .copy_dst = true },
+    //     .size = buffer_size * @sizeOf(f32),
+    //     .mapped_at_creation = .false,
+    // });
+
+    // core.queue.writeBuffer(output, 0, &[_]f32{5} ** buffer_size);
+
+    // const staging = core.device.createBuffer(&.{
+    //     .label = "staging buffer",
+    //     .usage = .{ .map_read = true, .copy_dst = true },
+    //     .size = buffer_size * @sizeOf(f32),
+    //     .mapped_at_creation = .false,
+    // });
 
     const shader_module = core.device.createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
     defer shader_module.release();
@@ -60,7 +105,9 @@ pub fn init(app: *App) !void {
             .label = "compute bind group",
             .layout = compute_pipeline_layout,
             .entries = &.{
-                gpu.BindGroup.Entry.buffer(0, output, 0, buffer_size * @sizeOf(f32)),
+                gpu.BindGroup.Entry.buffer(0, workgroup_buffer, 0, size),
+                gpu.BindGroup.Entry.buffer(1, local_buffer, 0, size),
+                gpu.BindGroup.Entry.buffer(2, global_buffer, 0, size),
             },
         }),
     );
@@ -130,55 +177,6 @@ pub fn init(app: *App) !void {
         }),
     );
 
-    // encoder.release();
-
-    // const queue = core.queue;
-    // const encoder = core.device.createCommandEncoder(null);
-
-    // TODO this wouldn't work in update.
-    // encoder.copyBufferToBuffer(output, 0, staging, 0, buffer_size * @sizeOf(f32));
-
-    // var command = encoder.finish(null);
-    // encoder.release();
-
-    // TODO handle better
-    // const allocator = gpa.allocator();
-    // const staging_response = try allocator.create(gpu.Buffer.MapAsyncStatus);
-    // staging_response.* = response;
-    // var response: gpu.Buffer.MapAsyncStatus = undefined;
-    // const callback = (struct {
-    //     pub inline fn callback(ctx: *gpu.Buffer.MapAsyncStatus, status: gpu.Buffer.MapAsyncStatus) void {
-    //         ctx.* = status;
-    //     }
-    // }).callback;
-    // staging.mapAsync(.{ .read = true }, 0, buffer_size * @sizeOf(f32), staging_response, callback);
-
-    // var response: gpu.Buffer.MapAsyncStatus = undefined;
-    // const callback = (struct {
-    //     pub inline fn callback(ctx: *gpu.Buffer.MapAsyncStatus, status: gpu.Buffer.MapAsyncStatus) void {
-    //         ctx.* = status;
-    //     }
-    // }).callback;
-
-    // queue.submit(&[_]*gpu.CommandBuffer{command});
-    // command.release();
-
-    // staging.mapAsync(.{ .read = true }, 0, buffer_size * @sizeOf(f32), &response, callback);
-    // while (true) {
-    //     if (response == gpu.Buffer.MapAsyncStatus.success) {
-    //         break;
-    //     } else {
-    //         core.device.tick();
-    //     }
-    // }
-
-    // const staging_mapped = staging.getConstMappedRange(f32, 0, buffer_size);
-    // for (staging_mapped.?) |v| {
-    //     std.debug.print("{d} ", .{v});
-    // }
-    // std.debug.print("\n", .{});
-    // staging.unmap();
-
     app.title_timer = try core.Timer.start();
     app.timer = try core.Timer.start();
     app.pipeline = core.device.createRenderPipeline(&pipeline_descriptor);
@@ -187,9 +185,10 @@ pub fn init(app: *App) !void {
     app.vertex_buffer = vertex_buffer;
     app.uniform_buffer = uniform_buffer;
     app.bind_group = bind_group;
-    app.output_buffer = output;
-    app.staging_buffer = staging;
-    // app.staging_response = staging_response;
+    app.read_buffers = [3]*gpu.Buffer{ workgroup_read_buffer, local_read_buffer, global_read_buffer };
+    app.write_buffers = [3]*gpu.Buffer{ workgroup_buffer, local_buffer, global_buffer };
+    // app.output_buffer = output;
+    // app.staging_buffer = staging;
 
     shader_module.release();
     pipeline_layout.release();
@@ -203,8 +202,9 @@ pub fn deinit(app: *App) void {
 
     app.vertex_buffer.release();
     app.uniform_buffer.release();
-    app.staging_buffer.release();
-    app.output_buffer.release();
+    // relaese buffers
+    // app.staging_buffer.release();
+    // app.output_buffer.release();
     app.bind_group.release();
     app.pipeline.release();
     app.compute_bind_group.release();
@@ -212,6 +212,7 @@ pub fn deinit(app: *App) void {
 }
 
 pub fn update(app: *App) !bool {
+    std.debug.print("{any}\n", .{app.read_buffers});
     var iter = core.pollEvents();
     while (iter.next()) |event| {
         switch (event) {
@@ -270,16 +271,21 @@ pub fn update(app: *App) !bool {
     const compute_pass = encoder.beginComputePass(null);
     compute_pass.setPipeline(app.compute_pipeline);
     compute_pass.setBindGroup(0, app.compute_bind_group, &.{});
-    compute_pass.dispatchWorkgroups(try std.math.divCeil(u32, buffer_size, workgroup_size), 1, 1);
+    // compute_pass.dispatchWorkgroups(try std.math.divCeil(u32, buffer_size, workgroup_size), 1, 1);
+    compute_pass.dispatchWorkgroups(4, 3, 2);
     compute_pass.end();
     compute_pass.release();
 
-    encoder.copyBufferToBuffer(app.output_buffer, 0, app.staging_buffer, 0, buffer_size * @sizeOf(f32));
+    for (0..3) |i| {
+        encoder.copyBufferToBuffer(app.write_buffers[i], 0, app.read_buffers[i], 0, size);
+    }
+
+    // encoder.copyBufferToBuffer(app.output_buffer, 0, app.staging_buffer, 0, buffer_size * @sizeOf(f32));
 
     var command = encoder.finish(null);
     encoder.release();
 
-    var response: gpu.Buffer.MapAsyncStatus = undefined;
+    var responses: [3]gpu.Buffer.MapAsyncStatus = undefined;
     const callback = (struct {
         pub inline fn callback(ctx: *gpu.Buffer.MapAsyncStatus, status: gpu.Buffer.MapAsyncStatus) void {
             ctx.* = status;
@@ -289,23 +295,46 @@ pub fn update(app: *App) !bool {
     queue.submit(&[_]*gpu.CommandBuffer{command});
     command.release();
 
-    std.debug.print("mapping\n", .{});
-    app.staging_buffer.mapAsync(.{ .read = true }, 0, buffer_size * @sizeOf(f32), &response, callback);
+    for (0..3) |i| {
+        app.read_buffers[i].mapAsync(.{ .read = true }, 0, size, &responses[i], callback);
+    }
     while (true) {
-        if (response == gpu.Buffer.MapAsyncStatus.success) {
-            break;
-        } else {
-            core.device.tick();
+        var response = false;
+        for (responses) |r| {
+            response = response or (r == gpu.Buffer.MapAsyncStatus.success);
         }
+        if (response) break else core.device.tick();
     }
 
-    std.debug.print("printing\n", .{});
-    const staging_mapped = app.staging_buffer.getConstMappedRange(f32, 0, buffer_size);
-    for (staging_mapped.?) |v| {
+    // std.debug.print("{any}\n", .{app.read_buffers[0].*});
+    // TODO: these come out as null. Why?
+    const workgroup = app.read_buffers[0].getConstMappedRange(zm.Vec, 0, buffer_size);
+    const local = app.read_buffers[1].getConstMappedRange(zm.Vec, 0, buffer_size);
+    const global = app.read_buffers[2].getConstMappedRange(zm.Vec, 0, buffer_size);
+    std.debug.print("WORKGROUP: \n", .{});
+    for (workgroup.?) |v| {
+        std.debug.print("{d} ", .{v});
+    }
+    std.debug.print("\nLOCAL: \n", .{});
+    for (local.?) |v| {
+        std.debug.print("{d} ", .{v});
+    }
+    std.debug.print("\nGLOBAL: \n", .{});
+    for (global.?) |v| {
         std.debug.print("{d} ", .{v});
     }
     std.debug.print("\n", .{});
-    app.staging_buffer.unmap();
+
+    for (0..3) |i| {
+        app.read_buffers[i].unmap();
+    }
+
+    // const staging_mapped = app.staging_buffer.getConstMappedRange(f32, 0, buffer_size);
+    // for (staging_mapped.?) |v| {
+    //     std.debug.print("{d} ", .{v});
+    // }
+    // std.debug.print("\n", .{});
+    // app.staging_buffer.unmap();
 
     core.swap_chain.present();
     back_buffer_view.release();
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -14,10 +14,26 @@
     return vec4f(1, 0, 0, 1);
 }
 
-@group(0) @binding(0) var<storage, read_write> output: array<f32>;
+@group(0) @binding(0) var<storage, read_write> workgroupResult: array<vec3u>;
+@group(0) @binding(1) var<storage, read_write> localResult: array<vec3u>;
+@group(0) @binding(2) var<storage, read_write> globalResult: array<vec3u>;
 
-@compute @workgroup_size(1) fn computeSomething(
-    @builtin(global_invocation_id) id : vec3<u32>,
+@compute @workgroup_size(2, 3, 4) fn computeSomething(
+    @builtin(workgroup_id) workgroup_id: vec3<u32>,
+    @builtin(local_invocation_id) local_invocation_id: vec3<u32>,
+    @builtin(global_invocation_id) global_invocation_id: vec3<u32>,
+    @builtin(local_invocation_index) local_invoication_index: u32,
+    @builtin(num_workgroups) num_workgroups: vec3<u32>,
 ) {
-    output[id.x] = output[id.x] * 2.;
+    let workgroup_index =
+        workgroup_id.x +
+        workgroup_id.y * num_workgroups.x +
+        workgroup_id.z * num_workgroups.x * num_workgroups.y;
+
+    let global_invocation_index =
+        workgroup_index * 2 * 3 * 4 + local_invoication_index;
+
+    workgroupResult[global_invocation_index] = workgroup_id;
+    localResult[global_invocation_index] = local_invocation_id;
+    globalResult[global_invocation_index] = global_invocation_id;
 }
