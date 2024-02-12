b707b2a675acbd4bbe937debd7f15935ff7a0340
Dario Oddenino <branch13@gmail.com>
2024-02-12 14:58:31 +0100

    appending files

--- src/renderer.zig
+++ src/renderer.zig
@@ -4,50 +4,40 @@ const gpu = core.gpu;
 
 const App = @import("main.zig").App;
 
+// TODO mix: https://github.com/hexops/mach-core/blob/main/examples/deferred-rendering/main.zig
+// TODO with: https://github.com/Shridhar2602/WebGPU-Path-Tracer/blob/main/renderer.js
 pub const Renderer = struct {
     allocator: std.mem.Allocator,
-    pipeline: *gpu.RenderPipeline,
+
+    // Buffers
+
+    // Bind groups
+
+    // Bind group layouts
+
+    // Pipelines
+    compute_pipeline: *gpu.ComputePipeline,
+    render_pipeline: *gpu.RenderPipeline,
+
+    // Pipeline layouts
+
+    // Render pass descriptor
 
     pub fn init(allocator: std.mem.Allocator) !Renderer {
-        var shaders = std.ArrayList(*gpu.ShaderModule).init(allocator);
-        defer shaders.deinit();
+        var shader_file = std.ArrayList(u8).init(allocator);
+        defer shader_file.deinit();
         const shader_files = .{ "header", "common", "main", "shootRay", "hitRay", "traceRay", "scatterRay", "importanceSampling" };
-        // const shader_module = core.device.createShaderModuleWGSL("triangle.wgsl", @embedFile("./shaders/triangle.wgsl"));
-        // defer shader_module.release();
-        // TODO how do I concatenate strings??
         const ext = ".wgsl";
         const folder = "./shaders/";
         inline for (shader_files) |file| {
             const file_name = file ++ ext;
             const file_folder = folder ++ file_name;
 
-            const shader_module = core.device.createShaderModuleWGSL(file, @embedFile(file_folder));
-            try shaders.append(shader_module);
-            defer shader_module.release();
+            const shader_file_content = @embedFile(file_folder);
+            try shader_file.appendSlice(shader_file_content);
         }
-        // for (0..shader_files.len) |i| {
-        //     // const file: []const u8 = try std.fmt.allocPrint(allocator, "{s}{s}", .{ shader_files[i], ext });
-        //     // defer allocator.free(file);
-        //     // const folder_file = try std.fmt.allocPrint(allocator, "{s}{s}", .{ folder, file });
-        //     // defer allocator.free(folder_file);
-        //     // const file_f: [*:0]const u8 = file[0..];
-        //     // const folder_file_f: [*:0]const u8 = folder_file[0..];
-        //     const file = try allocator.alloc(u8, shader_files[i].len + ext.len + 1);
-        //     std.mem.copyForwards(u8, file, shader_files[i]);
-        //     // file[shader_files[i].len] = ext[0..];
-        //     for (0..ext.len) |j| {
-        //         file[shader_files[i].len + j] = ext[j];
-        //     }
-        //     const file_folder = try allocator.alloc(u8, folder.len + file.len + 1);
-        //     std.mem.copyForwards(u8, file_folder, folder);
-        //     for (0..file.len) |j| {
-        //         file_folder[folder.len + j] = file[j];
-        //     }
-
-        //     const shader_module = core.device.createShaderModuleWGSL(file, @embedFile(file_folder));
-        //     shaders.append(shader_module);
-        //     defer shader_module.release();
-        // }
+        const shader_module = core.device.createShaderModuleWGSL("hooray", try shader_file.toOwnedSliceSentinel(0));
+        defer shader_module.release();
 
         // Fragment state
         const blend = gpu.BlendState{};
@@ -57,15 +47,11 @@ pub const Renderer = struct {
             .write_mask = gpu.ColorWriteMaskFlags.all,
         };
 
-        const shader_module = core.device.createShaderModule(.{
-            shaders.items,
-            shaders.items.len,
-        });
         const fragment = gpu.FragmentState.init(.{ .module = shader_module, .entry_point = "frag_main", .targets = &.{color_target} });
         const pipeline_descriptor = gpu.RenderPipeline.Descriptor{ .fragment = &fragment, .vertex = gpu.VertexState{ .module = shader_module, .entry_point = "vertex_main" } };
         const pipeline = core.device.createRenderPipeline(&pipeline_descriptor);
 
-        return Renderer{ .pipeline = pipeline };
+        return Renderer{ .pipeline = pipeline, .allocator = allocator };
     }
 
     pub fn deinit(self: *Renderer) void {
